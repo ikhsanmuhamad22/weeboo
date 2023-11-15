7802906854be648589956bcdccb3b933aa80547b
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-16 06:07:18 +0700

    update structure folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import AnimeList from "./components/AnimeList";
+import AnimeList from "../components/AnimeList";
 
 export default async function Home() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
